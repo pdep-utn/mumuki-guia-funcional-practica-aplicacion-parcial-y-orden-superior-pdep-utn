831ffa56c9a299381f84b917c0a05a4427ce65fd
julian@mumuki.org <julian@mumuki.org>
2020-04-15 17:16:10 +0000

    Mumuki Export on 2020-04-15 17:16:10 +0000

--- 00112_mejor/test.hs
+++ 00112_mejor/test.hs
@@ -2,5 +2,5 @@ describe "mejor" $ do
   it "devuelve 3 dados (**2) (*3) 1" $ do
     mejor (**2) (*3) 1`shouldBe` 3
 
-  it "devuelve 25 dados (**2) (*3) 1" $ do
+  it "devuelve 25 dados (**2) (*3) 5" $ do
     mejor (**2) (*3) 5`shouldBe` 25
